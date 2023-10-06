a28195e3edb207cfc3169f1bdf7e7a4fbd274dc2
Gary Yu <hyu100@my.bcit.ca>
2023-10-06 13:52:31 -0700

    allow for display of form data information in the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 import Image from 'next/image'
 
 //this method allows you to accomplish things with forms even with client-side Javascript disabled.
 //in next JS, you can just pass a function into the form action
 //this is a very new feature in Next, however, so you need to go directly to next.config.js
 //inside, you need to change nextConfig
+
+const cars: string[] = []
+
 async function createCar(data: FormData) {
   //this function triggers inside the backend console, rather than the client's
   "use server"
   // formData class
   console.log("car name", data.get("car-name"))
+  cars.push(data.get("car-name") as string)
+
+  //refresh the cache
+  revalidatePath("/")
+  //less control than when we handled routes with Express, but we still have enough control to generally do what we want to do
+  //redirect("/car")
 
 }
 
@@ -17,6 +28,7 @@ export default function Home() {
   return (
     <main className="max-w-4xl m-auto">
       <h1 className="text-3xl my-10">Cars</h1>
+      {JSON.stringify(cars)}
       <form action ={createCar} >
         <input 
           type="text"
